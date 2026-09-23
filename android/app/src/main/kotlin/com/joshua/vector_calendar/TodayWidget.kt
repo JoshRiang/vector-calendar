261480550fde87126295642fc47d48ecccf7f12b
Joshua Riangkamang <sandinya1sampai8@gmail.com>
2026-09-23 14:54:01 +0700

    Add missing java.net/java.io imports (fixes CI compile error)

--- android/app/src/main/kotlin/com/joshua/vector_calendar/TodayWidget.kt
+++ android/app/src/main/kotlin/com/joshua/vector_calendar/TodayWidget.kt
@@ -8,6 +8,11 @@ import android.content.Intent
 import android.widget.RemoteViews
 import org.json.JSONArray
 import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.net.HttpURLConnection
+import java.net.URL
 import java.util.Calendar
 import kotlin.concurrent.thread
 
